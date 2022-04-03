2c7e141b1576443fe0be9896f191856494e459e7
ApEdu <gargapoorv12@gmail.com>
2022-04-03 12:31:59 +0530

    Modal completed
    - added battery slider in modal
    - added car type select

--- public/script.js
+++ public/script.js
@@ -1,6 +1,16 @@
+// color variables
+var start_color = '#CE3000';
+var end_color = '#47B700';
+// battery slider variables
+var batteryLvl = 50;
+var slider = document.getElementById('batteryValInp');
+var batteryDisplay = document.getElementById('batteryVal');
+// car type select variable
+var carType = 'hatchback';
+
 // CREDENTIALS
 var platform = new H.service.Platform({
-  'apikey': 'api-key-goes-here'
+  'apikey': 'IEt8dt3NQy3h3phRpCJ_XxK_rcmpHjSlsSZ0GlfBT8U'
 });
 
 // Obtain the default map types from the platform object:
@@ -2039,11 +2049,7 @@ function revGeocode(lat, lon) {
       var x = result.items[0].title;
       document.getElementById("status").innerHTML = '<h3>' + x + '</h3>';
       var i = x.split(",");
-      if (getNumber(i) == "Nothing") {
-        document.getElementById("contacts").innerHTML = "Nothing";
-      } else {
-        document.getElementById("contacts").innerHTML = getNumber(i);
-      }
+      document.getElementById("contacts").innerHTML = getNumber(i);
     } else if (result.items.length == 0) {
       document.getElementById("status").innerHTML = "<h1>funCharge</h1>";
     }
@@ -2051,6 +2057,87 @@ function revGeocode(lat, lon) {
 
   geocoder.reverseGeocode(geocodeParam, onResult, alert);
 }
+
+// mark positions button
+markPosition = () => {
+  // TODO: start and end position mark logic
+  document.getElementById('routeBtn').classList.remove("disabled")
+}
+
+// function to get gradient color
+getGradientColor = function (start_color, end_color, percent) {
+  // strip the leading # if it's there
+  start_color = start_color.replace(/^\s*#|\s*$/g, '');
+  end_color = end_color.replace(/^\s*#|\s*$/g, '');
+
+  // convert 3 char codes --> 6, e.g. `E0F` --> `EE00FF`
+  if (start_color.length == 3) {
+    start_color = start_color.replace(/(.)/g, '$1$1');
+  }
+
+  if (end_color.length == 3) {
+    end_color = end_color.replace(/(.)/g, '$1$1');
+  }
+
+  // get colors
+  var start_red = parseInt(start_color.substr(0, 2), 16),
+    start_green = parseInt(start_color.substr(2, 2), 16),
+    start_blue = parseInt(start_color.substr(4, 2), 16);
+
+  var end_red = parseInt(end_color.substr(0, 2), 16),
+    end_green = parseInt(end_color.substr(2, 2), 16),
+    end_blue = parseInt(end_color.substr(4, 2), 16);
+
+  // calculate new color
+  var diff_red = end_red - start_red;
+  var diff_green = end_green - start_green;
+  var diff_blue = end_blue - start_blue;
+
+  diff_red = ((diff_red * percent) + start_red).toString(16).split('.')[0];
+  diff_green = ((diff_green * percent) + start_green).toString(16).split('.')[0];
+  diff_blue = ((diff_blue * percent) + start_blue).toString(16).split('.')[0];
+
+  // ensure 2 digits by color
+  if (diff_red.length == 1) diff_red = '0' + diff_red
+  if (diff_green.length == 1) diff_green = '0' + diff_green
+  if (diff_blue.length == 1) diff_blue = '0' + diff_blue
+
+  return '#' + diff_red + diff_green + diff_blue;
+};
+
+var accentColor;
+// DOMcontentloaded function
+document.addEventListener('DOMContentLoaded', function () {
+  // battery slider
+  accentColor = getGradientColor(start_color, end_color, batteryLvl / 100)
+  batteryDisplay.style.color = accentColor
+  batteryDisplay.innerHTML = batteryLvl
+  // cartype
+  document.getElementById(carType).style.backgroundColor = accentColor;
+}, false);
+
+// car type select
+carSelect = (carSelected) => {
+  // console.log("car clicked " + carSelected)
+  if (carType != carSelected) {
+    document.getElementById('hatchback').style.backgroundColor = '#FFFFFF';
+    document.getElementById('sedan').style.backgroundColor = '#FFFFFF';
+    document.getElementById('SUV').style.backgroundColor = '#FFFFFF';
+  }
+  carType = carSelected
+  document.getElementById(carType).style.backgroundColor = accentColor;
+};
+
+
+// battery slider value update
+slider.addEventListener('input', (event) => {
+  // slider.style.color = getGradientColor('#FF0000', '#00FF00', slider.value / 100)
+  batteryLvl = slider.value
+  accentColor = getGradientColor(start_color, end_color, batteryLvl / 100)
+  document.getElementById(carType).style.backgroundColor = accentColor;
+  batteryDisplay.style.color = accentColor
+  batteryDisplay.innerHTML = slider.value
+});
 
 // Listening to current map location:
 function ClickListener(map) {
