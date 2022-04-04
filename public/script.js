91fab9113b567a312c9f0483f0c67ab8f5805e4b
pjflux2001 <f20180332@goa.bits-pilani.ac.in>
2022-04-04 18:52:44 +0530

    routing parameters made dynamic

--- public/script.js
+++ public/script.js
@@ -21,8 +21,8 @@ var map = new H.Map(
   document.getElementById('mapContainer'),
   defaultLayers.vector.normal.map,
   {
-    zoom: 10,
-    center: { lat: 28.6139, lng: 77.2090 }
+    zoom: 5,
+    center: { lat: 49.8, lng: 15.47 }
   }
 );
 
@@ -364,7 +364,7 @@ function distance(lat1, lon1, lat2, lon2, unit) {
 }
 
 function route() {
-  $.getJSON('https://router.hereapi.com/v8/routes?departureTime=any&origin='+str.split(',')[0]+','+str.split(',')[1]+'&ev[connectorTypes]=iec62196Type2Combo&transportMode=car&destination='+end.split(',')[0]+','+end.split(',')[1]+'&return=polyline&ev[freeFlowSpeedTable]=0,0.239,27,0.239,45,0.259,60,0.196,75,0.207,90,0.238,100,0.26,110,0.296,120,0.337,130,0.351,250,0.351&ev[trafficSpeedTable]=0,0.349,27,0.319,45,0.329,60,0.266,75,0.287,90,0.318,100,0.33,110,0.335,120,0.35,130,0.36,250,0.36&ev[auxiliaryConsumption]=1.8&ev[ascent]=9&ev[descent]=4.3&ev[makeReachable]=true&ev[initialCharge]=48&ev[maxCharge]=80&ev[chargingCurve]=0,239,32,199,56,167,60,130,64,111,68,83,72,55,76,33,78,17,80,1&ev[maxChargeAfterChargingStation]=72&apiKey=IEt8dt3NQy3h3phRpCJ_XxK_rcmpHjSlsSZ0GlfBT8U', function(data){
+  $.getJSON('https://router.hereapi.com/v8/routes?departureTime=any&origin='+str.split(',')[0]+','+str.split(',')[1]+'&ev[connectorTypes]=iec62196Type2Combo&transportMode=car&destination='+end.split(',')[0]+','+end.split(',')[1]+'&return=polyline,actions,instructions,summary,routeHandle,passthrough&ev[freeFlowSpeedTable]=0,0.239,27,0.239,45,0.259,60,0.196,75,0.207,90,0.238,100,0.26,110,0.296,120,0.337,130,0.351,250,0.351&ev[trafficSpeedTable]=0,0.349,27,0.319,45,0.329,60,0.266,75,0.287,90,0.318,100,0.33,110,0.335,120,0.35,130,0.36,250,0.36&ev[auxiliaryConsumption]=1.8&ev[ascent]=9&ev[descent]=4.3&ev[makeReachable]=true&ev[initialCharge]=48&ev[maxCharge]=80&ev[chargingCurve]=0,239,32,199,56,167,60,130,64,111,68,83,72,55,76,33,78,17,80,1&ev[maxChargeAfterChargingStation]=72&apiKey=IEt8dt3NQy3h3phRpCJ_XxK_rcmpHjSlsSZ0GlfBT8U', function(data){
     let sections = data["routes"][0]["sections"];
     let redChargeIcon = new H.map.Icon('assets/1.png'),
         greenChargeIcon = new H.map.Icon('assets/2.png'),
